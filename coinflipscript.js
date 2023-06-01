01bf7bd3b62c133f6a999cd3669658b314aef517
LeAnne B <branchleannelmb@gmail.com>
2023-06-01 12:08:47 -0400

    finishes up flip functionality

--- coinflipscript.js
+++ coinflipscript.js
@@ -7,8 +7,7 @@ var outputTextContainer = document.getElementById("outputtextcontainer");
 
 function flipCoin () {
 
-    //clear text area first
-	outputTextContainer.innerHTML = "";
+
     //console.log("Flip coin function has run"); 
     coinElement.classList.add("animate__animated");
 	coinElement.classList.add("animate__flip");
@@ -21,11 +20,25 @@ function flipCoin () {
 //prepping random number 0 or 1 
 //console.log(Math.floor(Math.random() * 2));
 var headOrTails = Math.floor(Math.random() * 2);
-console.log(headOrTails);
+//console.log(headOrTails);
 
 if (headOrTails === 1) {
+        //clear text area first
+	outputTextContainer.innerHTML = "";
     document.getElementById("coin").src="cointailsresized.png";
+    var resultTextEl = document.createElement("p");
+    resultTextEl.innerText = "Tails"
+    outputTextContainer.appendChild(resultTextEl);
 }
+else {
+        //clear text area first
+	outputTextContainer.innerHTML = "";
+    document.getElementById("coin").src="coinresized.png";
+    var resultTextEl = document.createElement("p");
+    resultTextEl.innerText = "Heads"
+    outputTextContainer.appendChild(resultTextEl);
+}
+
 
 });
 }
