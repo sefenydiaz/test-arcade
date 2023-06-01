e1ed914ed36c2b6a3a34bcd5e19f874e93b23ead
Tawhid <tawhidkamal5@gmail.com>
2023-06-01 13:21:45 -0400

    created thats hilarious button in dad joke js

--- icanhavejokes.js
+++ icanhavejokes.js
@@ -9,7 +9,7 @@ const options = {
 };
     console.log(options)
     try {
-    const response = await fetch(url, options);
+      const response = await fetch(url, options);
     
         if (response.status === 429) {
             document.getElementById("dadjokecontainer").innerHTML = "";
@@ -20,35 +20,49 @@ const options = {
         }
         else {
 
-            const result = await response.text();
+          const result = await response.text();
             console.log(result);
-            
-            const jokesConvertedObj = JSON.parse(result);
-	        console.log(jokesConvertedObj);
+          const jokesConvertedObj = JSON.parse(result);
+	          console.log(jokesConvertedObj);
 
             document.getElementById("dadjokecontainer").innerHTML = "";
             
-            var jokeElement = document.createElement("p");
+          var jokeElement = document.createElement("p");
             jokeElement.id = 'joke';
-            var joke = jokesConvertedObj.setup;
+            
+          var joke = jokesConvertedObj.setup;
             jokeElement.innerText = joke;
             document.getElementById("dadjokecontainer").appendChild(jokeElement);
 
             var deliveryEl = document.createElement("p");
-	        var deli = jokesConvertedObj.delivery;
-	        deliveryEl.innerText = deli;
-	        deliveryEl.id = 'delivery';
-	        document.getElementById("joke").appendChild(deliveryEl);
+	          var deli = jokesConvertedObj.delivery;
+	          
+            deliveryEl.innerText = deli;
+	          deliveryEl.id = 'delivery';
+	          document.getElementById("joke").appendChild(deliveryEl);
 
-            var againButton = document.createElement("button");
+            
+          var againButton = document.createElement("button");
       
             againButton.id = 'againbutton';
             againButton.innerText = "That wasn't funny...";
             document.getElementById("delivery").appendChild(againButton);
-            document.getElementById("againbutton").addEventListener('click', async () => {
-            await getJoke();
-            document.getElementById("jokeImage").style.display = "block";
+            
+          document.getElementById("againbutton").addEventListener('click', async () => {
+              await getJoke();
+              document.getElementById("jokeImage").style.display = "block";
         });
+
+        var hilariousButton = document.createElement("button");
+            hilariousButton.id = 'hilariousbutton';
+            hilariousButton.innerText = "That's hilarious!";
+            document.getElementById("delivery").appendChild(hilariousButton);
+            
+            hilariousButton.addEventListener('click', async () => {
+                await getJoke();
+            });
+      
+            
       makeStanley();
     }
   } catch (error) {
