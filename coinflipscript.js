545c0e06433bef684082bbd4570ee6d29290ad58
LeAnne B <branchleannelmb@gmail.com>
2023-06-01 11:14:07 -0400

    adds in structure in js and html for output text heads or tails

--- coinflipscript.js
+++ coinflipscript.js
@@ -1,13 +1,24 @@
 
 var coinElement = document.getElementById("coin");
+var outputTextContainer = document.getElementById("outputtextcontainer");
 //on click
 //animate__flip
 //and append textbox that says heads or tails 
 
 function flipCoin () {
+
+    //clear text area first
+	outputTextContainer.innerHTML = "";
     //console.log("Flip coin function has run"); 
     coinElement.classList.add("animate__animated");
 	coinElement.classList.add("animate__flip");
+
+    coinElement.addEventListener('animationend', (event) => {
+		event.stopPropagation();
+		coinElement.classList.remove("animate__animated");
+		coinElement.classList.remove("animate__shakeX");
+
+});
 }
 
 //listen for click
