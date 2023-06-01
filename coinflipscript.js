d84d4a7a7b341a7e3c81c212bc1961cc3e965964
LeAnne B <branchleannelmb@gmail.com>
2023-06-01 10:52:10 -0400

    adds in link for coin img in html and base JS to select the coin

--- coinflipscript.js
+++ coinflipscript.js
@@ -1,8 +1,12 @@
 
-
+var coinElement = document.getElementById("coin");
 //on click
 //animate__flip
 //and append textbox that says heads or tails 
 
+function flipCoin () {
+    console.log("Hello"); 
+}
 
 //listen for click
+coinElement.addEventListener('click', flipCoin); 
