45d9cc083f00de857a1824d6233855439f1669ed
LeAnne B <branchleannelmb@gmail.com>
2023-06-01 11:07:12 -0400

    adds in working coin flip functionality

--- coinflipscript.js
+++ coinflipscript.js
@@ -5,7 +5,7 @@ var coinElement = document.getElementById("coin");
 //and append textbox that says heads or tails 
 
 function flipCoin () {
-    console.log("Flip coin function has run"); 
+    //console.log("Flip coin function has run"); 
     coinElement.classList.add("animate__animated");
 	coinElement.classList.add("animate__flip");
 }
