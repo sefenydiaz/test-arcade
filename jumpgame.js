45034a0bde273be304b9fe66b7a7f063cbc3a170
Tawhid <tawhidkamal5@gmail.com>
2023-06-02 12:38:13 -0400

    added comparison function for hit detection

--- jumpgame.js
+++ jumpgame.js
@@ -22,5 +22,11 @@ var checkHit = setInterval(function(){
     var blockPosition = 
     parseInt(window.getComputedStyle(block).getPropertyValue('left'));
 
+    if (blockPosition<20 && blockPosition>0 && playerPosition>=125){
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert('you lose!');
+    }
+
 
 },10);
