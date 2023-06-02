0175a180f635e22f77c6c8c2877b68d6c2f79e06
LeAnne B <branchleannelmb@gmail.com>
2023-06-02 14:04:10 -0400

    adds in score increase/decrease

--- ponggamescript.js
+++ ponggamescript.js
@@ -2,6 +2,7 @@
 
 const INITIAL_VELOCITY = .025
 const VELOCITY_INCREASE = 0.000001
+const SPEED = .02
 
 
 
@@ -64,6 +65,7 @@ class Ball {
 class Paddle {
     constructor(paddleElem) {
         this.paddleElem = paddleElem
+        this.reset()
     }
 
     get position() {
@@ -75,11 +77,21 @@ class Paddle {
     set position(value) {
         this.paddleElem.style.setProperty("--position", value)
     }
+
+    reset() {
+        this.position = 50
+    }
+
+    update(delta, ballHeight) {
+        this.position += SPEED * delta * (ballHeight - this.position)
+    }
 }
 
 const ball = new Ball(document.getElementById("ball"))
 const playerPaddle = new Paddle(document.getElementById("player-paddle"))
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"))
+const playerScoreElem = document.getElementById("player-score")
+const computerScoreElem = document.getElementById("computer-score")
 
 function randomNumberBetween(min, max) {
     return Math.random() * (max- min) + min
@@ -93,10 +105,30 @@ function update(time) {
     if (lastTime != null) {
         const delta = time - lastTime
         ball.update(delta)
+        computerPaddle.update(delta, ball.y)
+
+        if (isLose()) handleLose() 
     }
+
     
     lastTime = time
     window.requestAnimationFrame(update)
+}
+
+function isLose() {
+    const rect = ball.rect()
+    return rect.right >= window.innerWidth || rect.left <= 0
+}
+
+function handleLose(){
+    const rect = ball.rect()
+    if (rect.right >= window.innerWidth) {
+        playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1
+    } else {
+        computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1
+    }
+    ball.reset()
+    computerPaddle.reset()
 }
 
 document.addEventListener("mousemove", e => {
