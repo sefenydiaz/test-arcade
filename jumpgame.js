0b54b9ba8a736b758aee971ef61c72c2c8796407
Tawhid <tawhidkamal5@gmail.com>
2023-06-02 12:08:25 -0400

    added if statement

--- jumpgame.js
+++ jumpgame.js
@@ -2,7 +2,10 @@ var user = document.getElementById('player');
 var block = document.getElementById('object');
 
 function jump(){
-    user.classList.add('animate');
+    if(user.classList != 'animate'){
+        user.classList.add('animate');
+    }
+    
     setTimeout(function (){
         user.classList.remove('animate')
     },480)
