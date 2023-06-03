29247fc7935b5490b082b348ee0561ec26bc6f47
LeAnne B <branchleannelmb@gmail.com>
2023-06-03 09:14:42 -0400

    adds ids to boxes to use with js and links in favgame script to main page

---
+++ favgamescript.js
@@ -0,0 +1,14 @@
+var dadJokePageClicks = 0;
+var magic8PageClicks = 0;
+var jumpGamePageClicks = 0 
+var pongGamePageClicks = 0;
+
+//add event listener, on click add to variable 
+
+document.getElementById("magic8").addEventListener('click');
+document.getElementById("dadjokes").addEventListener('click');
+document.getElementById("jumpgame").addEventListener('click');
+document.getElementById("ponggame").addEventListener('click');
+
+
+//local storage 
